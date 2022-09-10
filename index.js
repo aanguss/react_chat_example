2c82713dc2cff7c8bb4212f4d6a6cf6b2ea4580f
aanguss <aangus@gmail.com>
2022-09-10 11:29:42 -0700

    added broadcast instructions - completed and working

--- index.js
+++ index.js
@@ -10,13 +10,20 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+// io.on('connection', (socket) => {
+//     socket.broadcast.emit('hi');
+// });
+
 io.on('connection', (socket) => {
     console.log('a user connected');
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
-      });
+    });
+
     socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
+        io.emit('chat message', msg);
+        console.log('message: ' + msg);
     });
 });
 
